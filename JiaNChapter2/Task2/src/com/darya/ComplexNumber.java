2d1589469912738ada550dcbd906af7847c8a2c2
Dar'ya Kolpakova <daka.kolp01@gmail.com>
2018-05-19 00:38:18 +0300

    ComplexNumber

---
+++ JiaNChapter2/Task2/src/com/darya/ComplexNumber.java
@@ -0,0 +1,50 @@
+package com.darya;
+
+public class ComplexNumber {
+
+    private double x;
+    private double y;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.x = real;
+        this.y = imaginary;
+    }
+
+    public double real() {
+        return x;
+    }
+
+    public double imaginary() {
+        return y;
+    }
+
+    public double magnitude() {
+         return Math.sqrt(x*x + y*y);
+    }
+
+    @Override
+    public String toString() {
+        return "{" + x + "." + y + "}";
+    }
+
+    public static ComplexNumber add (ComplexNumber a, ComplexNumber b) {
+        return new ComplexNumber(a.x + b.x, a.y + b.y);
+    }
+
+    public ComplexNumber add(ComplexNumber a) {
+        return new ComplexNumber(this.x + a.x, this.y + a.y);
+    }
+    public static ComplexNumber muliply (ComplexNumber a, ComplexNumber b) {
+        return new ComplexNumber(a.x * b.x - a.y * b.y, a.x * b.x + a.y * b.y);
+    }
+
+    public ComplexNumber multiply(ComplexNumber a) {
+        return new ComplexNumber(this.x * a.x - this.y * a.y, this.x * a.x + this.y * a.y);
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- JiaNChapter2/Task2/src/com/darya/Main.java
+++
@@ -1,8 +0,0 @@
-package com.darya;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-
-}
