17524fe4bd819682f94553ed9e5f89575ac55192
Dar'ya Kolpakova <daka.kolp01@gmail.com>
2018-05-28 22:58:16 +0300

    FileViewer: have been done

--- JiaNChapter3/Task3/src/com/dakakolp/FileViewer.java
+++ JiaNChapter3/Task3/src/com/dakakolp/FileViewer.java
@@ -2,6 +2,9 @@ package com.dakakolp;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileViewer extends Frame implements ActionListener {
     String directory;
@@ -9,7 +12,6 @@ public class FileViewer extends Frame implements ActionListener {
 
     public FileViewer(String directory, String fileName) {
         super();
-
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 dispose();
@@ -25,10 +27,97 @@ public class FileViewer extends Frame implements ActionListener {
         p.setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 5));
         this.add("South", p);
 
-        Font font = new Font
+        Font font = new Font("SansSerif", Font.BOLD, 14);
+        Button openfile = new Button("Open file");
+        Button close = new Button("Close");
+        openfile.addActionListener(this);
+        openfile.setActionCommand("open");
+        openfile.setFont(font);
+        close.addActionListener(this);
+        close.setActionCommand("close");
+        close.setFont(font);
+        p.add(openfile);
+        p.add(close);
+
+        this.pack();
+
+        if(directory == null){
+            File f;
+            if((fileName != null) && (f = new File(fileName)).isAbsolute()) {
+                directory = f.getParent();
+                fileName = f.getName();
+            } else {
+                directory = System.getProperty("user.dir");
+            }
+        }
+        this.directory = directory;
+        setFile(directory, fileName);
+    }
+
+    private void setFile(String directory, String fileName) {
+        if((fileName == null) || (fileName.length() == 0)) return;
+
+        File f;
+
+        FileReader in = null;
+
+        try {
+            f = new File(directory, fileName);
+            in = new FileReader(f);
+            char[] buffer = new char[4096];
+            int len;
+            textArea.setText("");
+            while((len = in.read(buffer)) != -1) {
+                String s = new String(buffer, 0, len);
+                textArea.append(s);
+            }
+            this.setTitle("FileViewer: " + fileName);
+            textArea.setCaretPosition(0);
+        } catch (IOException e) {
+            textArea.setText(e.getClass().getName() + ": " + e.getMessage());
+            this.setTitle("FileViewer: " + fileName + ": IOException");
+        } finally {
+            try {
+                if (in != null)
+                    in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public FileViewer() {
         this(null, null);
     }
+
+    public FileViewer(String fileName) {
+        this(null, fileName);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String cmd = e.getActionCommand();
+        if(cmd.equals("open")) {
+            FileDialog f = new FileDialog(this, "Open File", FileDialog.LOAD);
+            f.setDirectory(directory);
+
+            f.show();
+            directory = f.getDirectory();
+            setFile(directory, f.getFile());
+            f.dispose();
+        } else if (cmd.equals("close"))
+            this.dispose();
+    }
+
+    public static void main(String[] args) {
+        Frame f = new FileViewer((args.length == 1) ? args[0] : null);
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        f.show();
+    }
+
 }
